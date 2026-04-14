b3b975ee1b6bd552ab7a8ae1d0ef1ab87c2d0bc5
Veljanovski <milosveljanovski@gmail.com>
2026-04-14 23:53:14 +0200

    Project setup

---
+++ src/aspire/Aspire.AppHost/AppHost.cs
@@ -0,0 +1,14 @@
+IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
+
+IResourceBuilder<PostgresDatabaseResource> database = builder
+    .AddPostgres("database")
+    .WithImage("postgres:17")
+    .WithBindMount("../../.containers/db", "/var/lib/postgresql/data")
+    .AddDatabase("clean-architecture");
+
+builder.AddProject<Projects.Web_RestApi>("web-restapi")
+    .WithEnvironment("ConnectionStrings__Database", database)
+    .WithReference(database)
+    .WaitFor(database);
+
+await builder.Build().RunAsync();
